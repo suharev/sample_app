c1a1bfeb8fd181c54abac9496a4e8610c230653e
Anton Suharev <antosha.suharev@gmail.com>
2012-05-12 16:01:01 +0200

    Add a title helper

--- Gemfile
+++ Gemfile
@@ -5,6 +5,7 @@ gem 'rails', '3.2.3'
 group :development, :test do
   gem 'sqlite3', '1.3.5'
   gem 'rspec-rails', '2.9.0'
+  gem 'guard-rspec'
 end
 
 # Gems used only for assets and not required
@@ -19,6 +20,8 @@ gem 'jquery-rails', '2.0.0'
 
 group :test do
   gem 'capybara', '1.1.2'
+  gem 'guard-spork', '0.3.2'
+  gem 'spork', '0.9.0'
 end
 
 group :production do
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -9,10 +9,9 @@ describe "Static pages" do
       page.should have_selector('h1', :text => 'Sample App')
     end
 
-    it "should have the title 'Home'" do
+    it "should not have a custom page title" do
       visit '/static_pages/home'
-      page.should have_selector('title',
-                        :text => "Ruby on Rails Tutorial Sample App | Home")
+      page.should_not have_selector('title', :text => '| Home')
     end
   end
 
